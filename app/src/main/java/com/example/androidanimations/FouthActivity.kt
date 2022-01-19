5bc780aa09a223329aba4971362fd86b8c76c7c2
prateekpandey1234 <prateek775999@gmail.com>
2022-01-19 16:35:02 +0530

    every animation so  far

--- app/src/main/java/com/example/androidanimations/FouthActivity.kt
+++ app/src/main/java/com/example/androidanimations/FouthActivity.kt
@@ -1,15 +1,70 @@
 package com.example.androidanimations
 
+import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_fourth.*
+import android.view.ViewAnimationUtils
+
+import android.animation.Animator
+import android.view.View
+import androidx.core.animation.addListener
+import kotlin.math.hypot
+
 
 class FouthActivity:AppCompatActivity() {
+    lateinit var animate1:AnimationDrawable
+    var clicked:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fourth)
+        clicked = 0
         //to show this animation ....add
         //android:transitionName="example_transition"
         //inside the xml code for the View you want to share between activities
+
+        //animation using icons
+        //store the animation inside the drawable file cuz it's a background resource
+        imageView2.setBackgroundResource(R.drawable.animation)
+        animate1= imageView2.background as AnimationDrawable
+
+//Floating Action button animation
+        fab2.setOnClickListener {
+            if (clicked%2==0){
+                //code for revealing animation
+                val cx: Int = it.width / 2
+                val cy: Int = it.height / 2
+
+                val finalRadius = hypot((cx).toDouble(), (cy).toDouble()).toFloat()
+
+                val anim = ViewAnimationUtils.createCircularReveal(it, cx, cy, 0f, finalRadius)
+                it.visibility = View.VISIBLE
+                clicked+=1
+                anim.start()
+            }
+            else{
+                //code for closing animation
+                val cx: Int = it.width / 2
+                val cy: Int = it.height / 2
+                clicked+=1
+                val finalRadius = hypot((cx).toDouble(), (cy).toDouble()).toFloat()
+
+                val anim = ViewAnimationUtils.createCircularReveal(it, cx, cy, finalRadius, 0f)
+                anim.addListener {Animator->
+                    it.visibility = View.INVISIBLE
+                }
+                anim.start()
+            }
+        }
+
+
+    }
+//The following method gets called when window's focus changes. For instance, if the window
+// gains/losses focus, it will be triggered. In case, the window gains focus, hasFocus is true and false otherwise.
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        animate1.start()
     }
 
 }
--- app/src/main/java/com/example/androidanimations/MainActivity.kt
+++ app/src/main/java/com/example/androidanimations/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.androidanimations
 
+import android.animation.ArgbEvaluator
+import android.animation.TimeAnimator
+import android.animation.ValueAnimator
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.daimajia.androidanimations.library.Techniques
@@ -27,6 +33,30 @@ class MainActivity : AppCompatActivity() {
 //            IMPORTANT
             //go to themes xml file to add animation to whole project instead of adding animation specifically
         }
+        //gradient animation for background of screen
+        //you need a gradient also in drawable file and use it as background for the layout you want
+        //unless it will show error
+        var gradient = layout1.background as GradientDrawable
+        val start = Color.DKGRAY
+        val mid = Color.MAGENTA
+        val end = Color.BLUE
+
+        val animator = TimeAnimator.ofFloat(0.0f, 1.0f)
+        animator.duration = 1500
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.repeatMode = ValueAnimator.REVERSE
+        val evaluator = ArgbEvaluator()
+        //This evaluator can be used to perform type interpolation between integer values that represent ARGB colors
+        animator.addUpdateListener {
+            val fraction = it.animatedFraction
+            val newStart = evaluator.evaluate(fraction, start, end) as Int
+            val newMid = evaluator.evaluate(fraction, mid, start) as Int
+            val newEnd = evaluator.evaluate(fraction, end, mid) as Int
+
+            gradient.colors = intArrayOf(newStart, newMid, newEnd)
+        }
+
+        animator.start()
 
 
     }
